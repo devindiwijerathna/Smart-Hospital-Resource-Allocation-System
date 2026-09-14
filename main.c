094c6b032f2c0ac8e4cd39fd0c996081139a2700
Devindi <dewindiwijerathne@gmail.com>
2026-09-14 22:00:06 +0530

    Commit 1 - Initial project setup

---
+++ main.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_PATIENTS 100
+#define NUM_SPECIALTIES 4
+#define NUM_WARDS 4
+#define MAX_BEDS 20
+
+/* Specialty lookup data */
+const char specialtyNames[NUM_SPECIALTIES][30] = {
+    "General Practice",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
+};
+
+const float consultationFee[NUM_SPECIALTIES] = {
+    1500.00,
+    2500.00,
+    4500.00,
+    5000.00
+};
+
+const int consultationTime[NUM_SPECIALTIES] = {
+    15,
+    20,
+    30,
+    30
+};
+
+/* Ward lookup data */
+const char wardNames[NUM_WARDS][30] = {
+    "General Ward",
+    "Paediatric Ward",
+    "Surgical Ward",
+    "ICU"
+};
+
+const float wardRate[NUM_WARDS] = {
+    3000.00,
+    6000.00,
+    12000.00,
+    25000.00
+};
+
+const int wardCapacity[NUM_WARDS] = {
+    20,
+    10,
+    10,
+    5
+};
+
+/* Bed occupancy */
+int bedOccupancy[NUM_WARDS][MAX_BEDS] = {0};
+
+/* Patient data */
+char patientName[MAX_PATIENTS][100];
+char patientID[MAX_PATIENTS][20];
+
+int patientAge[MAX_PATIENTS];
+int urgencyLevel[MAX_PATIENTS];
+int patientSpecialty[MAX_PATIENTS];
+int admitted[MAX_PATIENTS];
+int patientWard[MAX_PATIENTS];
+int daysAdmitted[MAX_PATIENTS];
+int assignedBed[MAX_PATIENTS];
+
+int waitingTime[MAX_PATIENTS];
+int specialtyQueue[NUM_SPECIALTIES];
+
+int patientCount = 0;
+
+
+/* Display main menu */
+void displayMenu()
+{
+    printf("\n=====================================\n");
+    printf("       SMART HOSPITAL SYSTEM\n");
+    printf("=====================================\n");
+    printf("1. Register Patient\n");
+    printf("2. Display Patients\n");
+    printf("3. Search Patient\n");
+    printf("4. Allocate Bed\n");
+    printf("5. Calculate Bill\n");
+    printf("6. Display Reports\n");
+    printf("7. Exit\n");
+    printf("=====================================\n");
+}
+
+
+/* Display specialties */
+void displaySpecialties()
+{
+    int i;
+
+    printf("\n---------- SPECIALTIES ----------\n");
+
+    for(i = 0; i < NUM_SPECIALTIES; i++)
+    {
+        printf("%d. %s - Rs. %.2f - %d minutes\n",
+               i + 1,
+               specialtyNames[i],
+               consultationFee[i],
+               consultationTime[i]);
+    }
+}
+
+
+/* Display wards */
+void displayWards()
+{
+    int i;
+
+    printf("\n---------- WARDS ----------\n");
+
+    for(i = 0; i < NUM_WARDS; i++)
+    {
+        printf("%d. %s - Rs. %.2f/day - Capacity: %d\n",
+               i + 1,
+               wardNames[i],
+               wardRate[i],
+               wardCapacity[i]);
+    }
+}
+
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        displayMenu();
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("\nPatient registration will be added next.\n");
+                break;
+
+            case 2:
+                printf("\nPatient display will be added next.\n");
+                break;
+
+            case 3:
+                printf("\nPatient search will be added next.\n");
+                break;
+
+            case 4:
+                printf("\nBed allocation will be added next.\n");
+                break;
+
+            case 5:
+                printf("\nBill calculation will be added next.\n");
+                break;
+
+            case 6:
+                printf("\nReports will be added next.\n");
+                break;
+
+            case 7:
+                printf("\nThank you for using Smart Hospital System!\n");
+                break;
+
+            default:
+                printf("\nInvalid choice! Please enter 1-7.\n");
+        }
+
+    } while(choice != 7);
+
+    return 0;
+}
